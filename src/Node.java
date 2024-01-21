768fd5130f0faa745ceae55dd27977be30aa746e
Ove <Ove@Ove.lan>
2024-01-21 14:02:13 +0200

    Initial commit: Node class created.

---
+++ src/Node.java
@@ -0,0 +1,37 @@
+public class Node {
+	private int data;
+	private Node next;
+	private Node prev;
+	
+	// Constructor	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	// Getters and setters
+	public int getData() {
+		return data;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}	
+	
+	public Node getPrev() {
+		return prev;
+	}
+	
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+}
